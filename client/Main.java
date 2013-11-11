af4575069671c558f79235c9125468c574135243
whittin3 <whittin3@gmail.com>
2013-11-10 21:17:17 -0600

    Great starting point for others to start. I showed how to add css templates and how to parse the config.

--- client/Main.java
+++ client/Main.java
@@ -14,8 +14,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("welcome.fxml"));
+        root.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        primaryStage.setTitle("Welcome to Quiqour");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
