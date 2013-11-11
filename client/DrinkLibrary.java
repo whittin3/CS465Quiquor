b23514d1005456e3df6c8848cd3b73c12eee1a7c
whittin3 <whittin3@gmail.com>
2013-11-10 20:36:03 -0600

    Added some functions parsing functions

---
+++ client/DrinkLibrary.java
@@ -0,0 +1,97 @@
+package client;
+
+/**
+ * User: Neal Eric
+ * Date: 11/10/13
+ */
+
+import client.readOnly.Drink;
+import client.readOnly.Ingredient;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+public class DrinkLibrary {
+
+    public static final String LAST_BAR_SETUP_TXT = "LastBarSetup.txt";
+    public HashMap<String, Ingredient> ingredients = new HashMap<>();
+    public List<Drink> drinkList = new ArrayList<>();
+
+
+    public void addDrink(String name, HashMap<Ingredient, Double> ingredientMap)
+    {
+        Drink newDrink = new Drink(name, ingredientMap);
+        drinkList.add(newDrink);
+    }
+
+
+    public ObservableList<String> parseLastBarSetup(String path) throws URISyntaxException, FileNotFoundException {
+        File file = new File(path);
+        ObservableList<String> ingredientPumpList = FXCollections.observableArrayList();
+        if (file.exists()) {
+            try (Scanner scanner = new Scanner(file).useDelimiter("[\\n]")) {
+                while (scanner.hasNext()) {
+                    ingredientPumpList.add(scanner.next().replaceAll("\\r", ""));
+                }
+            }
+        }
+        return ingredientPumpList;
+    }
+
+    public static DrinkLibrary parseDrinkFile() throws FileNotFoundException, URISyntaxException {
+        File file = new File("DrinkLibrary.csv");
+        DrinkLibrary drinkLibrary = new DrinkLibrary();
+        try (Scanner scanner = new Scanner(file).useDelimiter("[\\n]+")) {
+            boolean errorFlag = false;
+            while (scanner.hasNext()) {
+                String line = scanner.next();
+                Scanner linescanner = new Scanner(line.replace("\r", ",")).useDelimiter("\\s*,\\s*");
+                String DrinkName = linescanner.next();
+                HashMap<Ingredient, Double> ingredientMap = new HashMap<>();
+                while (linescanner.hasNext()) {
+                    String ingredientName = linescanner.next();
+                    if (ingredientName.equals("")) {
+                        break;
+                    }
+                    String volume = linescanner.next();
+                    if (volume.equals("")) {
+                        System.out.println("IGNORING DRINK");
+                        System.out.println("Please enter a volume for " + ingredientName + " in " + DrinkName);
+                        errorFlag = true;
+                        break;
+                    }
+
+                    Ingredient ingredient = drinkLibrary.addOrFindIngredient(ingredientName, 1, false);
+                    ingredientMap.put(ingredient, Double.valueOf(volume));
+                }
+                if (!errorFlag && !ingredientMap.isEmpty()) {
+                    drinkLibrary.addDrink(DrinkName, ingredientMap);
+                }
+                else {
+                    if (ingredientMap.isEmpty()) {
+                        System.out.println("IGNORING DRINK");
+                        System.out.println("Please enter ingredients for " + DrinkName);
+                    }
+                    errorFlag = false;
+                }
+            }
+        }
+        return drinkLibrary;
+    }
+
+    private Ingredient addOrFindIngredient(String name, double density, boolean carbonated) {
+        if (!ingredients.containsKey(name)) {
+            Ingredient ingredient = new Ingredient(name, density, carbonated);
+            ingredients.put(name, ingredient);
+        }
+
+        return ingredients.get(name);
+    }
+}
--- client/UserSettings.java
+++ client/UserSettings.java
@@ -1,8 +1,62 @@
 package client;
 
+import java.io.*;
+import java.net.URISyntaxException;
+import java.security.CodeSource;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * User: Neal Eric
  * Date: 11/10/13
  */
 public class UserSettings {
+    private Boolean StartInFullScreen;
+    private Boolean EnableAddNewDrinkButton;
+    private Boolean AlwaysStartWithAnEmptyBar;
+
+    public void parseConfigFile() throws FileNotFoundException, URISyntaxException {
+        CodeSource codeSource = Main.class.getProtectionDomain().getCodeSource();
+        String path = new File(codeSource.getLocation().toURI().getPath()).getParentFile().getAbsolutePath().replaceAll("\\\\", "/");
+        File file = new File(path + "/config.txt");
+        if (file.exists()) {
+            try (Scanner scanner = new Scanner(file).useDelimiter("[=\\n]")) {
+                boolean errorFlag = false;
+                while (scanner.hasNext()) {
+                    String line = scanner.next().replaceAll("\\s", "");
+                    if (line.equals("StartInFullScreen")) {
+                        StartInFullScreen = Boolean.valueOf(scanner.next().replaceAll("\\s", ""));
+                    }
+                    if (line.equals("EnableAddNewDrinkButton")) {
+                        EnableAddNewDrinkButton = Boolean.valueOf(scanner.next().replaceAll("\\s", ""));
+                    }
+                    if (line.equals("AlwaysStartWithAnEmptyBar")) {
+                        AlwaysStartWithAnEmptyBar = Boolean.valueOf(scanner.next().replaceAll("\\s", ""));
+                    }
+                }
+            }
+        }
+        else {
+            try {
+                writeConfigFile();
+            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public void writeConfigFile() throws URISyntaxException, IOException {
+        CodeSource codeSource = Main.class.getProtectionDomain().getCodeSource();
+        String path = new File(codeSource.getLocation().toURI().getPath()).getParentFile().getAbsolutePath().replaceAll("\\\\", "/");
+        FileWriter fstream = new FileWriter(path + "/config.txt");
+        try (BufferedWriter out = new BufferedWriter(fstream)) {
+            out.write("StartInFullScreen = " + String.valueOf(StartInFullScreen));
+            out.newLine();
+            out.write("EnableAddNewDrinkButton = " + String.valueOf(EnableAddNewDrinkButton));
+            out.newLine();
+            out.write("AlwaysStartWithAnEmptyBar = " + String.valueOf(AlwaysStartWithAnEmptyBar));
+            out.newLine();
+        }
+    }
 }
