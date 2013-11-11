ae4bae911ccbbf735bbfc6e27218ebe1741e0793
whittin3 <whittin3@gmail.com>
2013-11-11 12:30:25 -0600

    GUIDrinkController has the correct orientation and re-sizes smoothly

--- client/GUIDrinkController.java
+++ client/GUIDrinkController.java
@@ -23,39 +23,45 @@ import java.util.List;
 
 public class GUIDrinkController {
     List<String> ingredientList;
+    private final VBox vBox = new VBox();
 
     public GUIDrinkController() {
         ingredientList = new ArrayList<>();
     }
 
     public GUIDrinkController(Drink drink) {
-        for (Ingredient ingredient : drink.getIngredients())
-        {
+        for (Ingredient ingredient : drink.getIngredients()) {
             addIngredient(ingredient);
         }
     }
-    public void addIngredient(Ingredient ingredient)
-    {
+
+    public void addIngredient(Ingredient ingredient) {
         ingredientList.add(ingredient.getName());
 
     }
 
     public void init(Stage primaryStage) {
-        final VBox root = new VBox();
-        primaryStage.setScene(new Scene(root, 600, 675));
+        primaryStage.setScene(new Scene(vBox, 600, 675));
+        vBox.setAlignment(Pos.BOTTOM_CENTER);
 //        String css = Main.class.getResource("default.css").toExternalForm();
-        root.getChildren().add(new Cell(Color.ALICEBLUE));
-        root.getChildren().add(new Cell(Color.BISQUE));
+        vBox.getChildren().add(new Cell(Color.DARKGREEN));
+        vBox.getChildren().add(new Cell(Color.GREEN));
 
     }
 
+    /**
+     * Each Cell is a (drink) row in the GUIDrinkController
+     */
     private static class Cell extends StackPane {
 
         private static final int MAX_WIDTH = 100;
+        private static final double MIN_HEIGHT = 20;
+        private static final int STARTING_HEIGHT = 310;
+        private static final int HORIZONTAL_MARGIN = 7;
         private double y;
         private boolean dragging;
-        private Rectangle rectangle = new Rectangle(MAX_WIDTH,210);
-        private Text text = new Text("sdsd");
+        private Rectangle rectangle = new Rectangle(MAX_WIDTH, STARTING_HEIGHT);
+        private Text text = new Text("Test Drink");
 
         private Cell(Color color) {
             super();
@@ -63,13 +69,15 @@ public class GUIDrinkController {
             stylize(color);
         }
 
-        private void stylize(Color color)
-        {
+        private void stylize(Color color) {
+            //Stackpane
             this.setAlignment(Pos.TOP_CENTER);
             this.setMaxWidth(MAX_WIDTH);
 
+            //Rectangle
             rectangle.setFill(color);
 
+            //Text
         }
 
         public void init(final Cell cell) {
@@ -106,37 +114,38 @@ public class GUIDrinkController {
         }
 
         private void mouseOver(MouseEvent event) {
-            if(isInResizableMargin(event) || dragging) {
+            if (isInResizableMargin(event) || dragging) {
                 this.setCursor(Cursor.S_RESIZE);
-            }
-            else {
+            } else {
                 this.setCursor(Cursor.DEFAULT);
             }
         }
 
         private boolean isInResizableMargin(MouseEvent event) {
-            return event.getY() > (this.getHeight() - 7);
+            return event.getY() <= (HORIZONTAL_MARGIN);
         }
 
         private void mouseDragged(MouseEvent event) {
-            if(!dragging) {
+            if (!dragging) {
                 return;
             }
 
-            double mouseY = event.getY();
-            double newHeight = rectangle.getHeight() + mouseY - y;
-            rectangle.setHeight(newHeight);
-            y = mouseY;
+            double mouseY = event.getSceneY();
+            double newHeight = rectangle.getHeight() - mouseY + y;
+            if (newHeight > MIN_HEIGHT) {
+                rectangle.setHeight(newHeight);
+                y = mouseY;
+            }
         }
 
         private void mousePressed(MouseEvent event) {
 
             // @todo Create a DraggableZone
-            if(!isInResizableMargin(event)) {
+            if (!isInResizableMargin(event)) {
                 return;
             }
             dragging = true;
-            y = event.getY();
+            y = event.getSceneY();
         }
     }
 }
