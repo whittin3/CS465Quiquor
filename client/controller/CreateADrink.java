a8d0345f8cc963ea1b6f563a8cff5db612479acb
whittin3 <whittin3@gmail.com>
2013-12-08 02:04:47 -0600

    Some Styling to get the ball rolling for other to contribute

--- client/controller/CreateADrink.java
+++ client/controller/CreateADrink.java
@@ -6,8 +6,11 @@ import client.ViewController;
 import client.transitions.FadeTransition;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.geometry.Bounds;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextBuilder;
 
@@ -43,13 +46,19 @@ public class CreateADrink implements View {
 		viewController.setScreen(ViewController.Home, new FadeTransition());
 	}
 
-	private static class IngredientCell extends AnchorPane {
+	private static class IngredientCell extends StackPane {
 
 		public IngredientCell(String ingredient) {
-			int index = Main.getIngredients().indexOf(ingredient);
-//			Text text = TextBuilder.create().text(ingredient).styleClass("ingredient" + String.valueOf(index)).build();
 			Text text = TextBuilder.create().text(ingredient).styleClass("ingredientItem").build();
+			Bounds rectBounds = text.getBoundsInParent();
+
+			Rectangle rectangle = new Rectangle(rectBounds.getWidth(), rectBounds.getHeight());
+			int index = Main.getIngredients().indexOf(ingredient);
+			rectangle.getStyleClass().setAll("drink-cell-" + String.valueOf(index));
+			getChildren().add(rectangle);
+
 			getChildren().add(text);
+
 		}
 	}
 }
