9ada5c97728ad1037a15dd754892dcde7d9d48aa
ramitarora <arora15@illinois.edu>
2013-12-09 05:35:04 -0600

    Committing for Ali Sakr. Fixes in Drink Library

--- client/DrinkLibrary.java
+++ client/DrinkLibrary.java
@@ -21,92 +21,93 @@ import java.util.Scanner;
  */
 public class DrinkLibrary {
 
-	public static final String LAST_BAR_SETUP_TXT = "LastBarSetup.txt";
-	public HashMap<String, Ingredient> ingredients = new HashMap<>();
-	public HashMap<String, Drink> drinks = new HashMap<>();
+    public static final String LAST_BAR_SETUP_TXT = "LastBarSetup.txt";
+    public HashMap<String, Ingredient> ingredients = new HashMap<>();
+    public HashMap<String, Drink> drinks = new HashMap<>();
 
-	public HashMap<String, Ingredient> getIngredients() {
-		return ingredients;
-	}
+    public HashMap<String, Ingredient> getIngredients() {
+        return ingredients;
+    }
 
-	public void addDrink(String name, HashMap<Ingredient, Double> ingredientMap) {
-		Drink newDrink = new Drink(name, ingredientMap);
-		drinks.put(newDrink.getName(), newDrink);
-	}
+    public void addDrink(String name, HashMap<Ingredient, Double> ingredientMap) {
+        Drink newDrink = new Drink(name, ingredientMap);
+        drinks.put(newDrink.getName(), newDrink);
+    }
 
-	public ObservableList<String> parseLastBarSetup(String path) throws URISyntaxException, FileNotFoundException {
-		File file = new File(path);
-		ObservableList<String> ingredientPumpList = FXCollections.observableArrayList();
-		if (file.exists()) {
-			try (Scanner scanner = new Scanner(file).useDelimiter("[\\n]")) {
-				while (scanner.hasNext()) {
-					ingredientPumpList.add(scanner.next().replaceAll("\\r", ""));
-				}
-			}
-		}
-		return ingredientPumpList;
-	}
+    public ObservableList<String> parseLastBarSetup(String path) throws URISyntaxException, FileNotFoundException {
+        File file = new File(path);
+        ObservableList<String> ingredientPumpList = FXCollections.observableArrayList();
+        if (file.exists()) {
+            try (Scanner scanner = new Scanner(file).useDelimiter("[\\n]")) {
+                while (scanner.hasNext()) {
+                    ingredientPumpList.add(scanner.next().replaceAll("\\r", ""));
+                }
+            }
+        }
+        return ingredientPumpList;
+    }
 
-	public static DrinkLibrary parseDrinkFile() throws FileNotFoundException, URISyntaxException {
-		File file = new File("src/data/DrinkLibrary.csv");
-		DrinkLibrary drinkLibrary = new DrinkLibrary();
-		try (Scanner scanner = new Scanner(file).useDelimiter("[\\n]+")) {
-			boolean errorFlag = false;
-			while (scanner.hasNext()) {
-				String line = scanner.next();
-				Scanner linescanner = new Scanner(line.replace("\r", ",")).useDelimiter("\\s*,\\s*");
-				String DrinkName = linescanner.next();
-				HashMap<Ingredient, Double> ingredientMap = new HashMap<>();
-				while (linescanner.hasNext()) {
-					String ingredientName = linescanner.next();
-					if (ingredientName.equals("")) {
-						break;
-					}
-					String volume = linescanner.next();
-					if (volume.equals("")) {
-						System.out.println("IGNORING DRINK");
-						System.out.println("Please enter a volume for " + ingredientName + " in " + DrinkName);
-						errorFlag = true;
-						break;
-					}
+    public static DrinkLibrary parseDrinkFile() throws FileNotFoundException, URISyntaxException {
+        String fullFilePath = Main.class.getResource("../data/DrinkLibrary.csv").getFile();
+        File file = new File(fullFilePath);
+        DrinkLibrary drinkLibrary = new DrinkLibrary();
+        try (Scanner scanner = new Scanner(file).useDelimiter("[\\n]+")) {
+            boolean errorFlag = false;
+            while (scanner.hasNext()) {
+                String line = scanner.next();
+                Scanner linescanner = new Scanner(line.replace("\r", ",")).useDelimiter("\\s*,\\s*");
+                String DrinkName = linescanner.next();
+                HashMap<Ingredient, Double> ingredientMap = new HashMap<>();
+                while (linescanner.hasNext()) {
+                    String ingredientName = linescanner.next();
+                    if (ingredientName.equals("")) {
+                        break;
+                    }
+                    String volume = linescanner.next();
+                    if (volume.equals("")) {
+                        System.out.println("IGNORING DRINK");
+                        System.out.println("Please enter a volume for " + ingredientName + " in " + DrinkName);
+                        errorFlag = true;
+                        break;
+                    }
 
-					Ingredient ingredient = drinkLibrary.addOrFindIngredient(ingredientName, 1, false);
-					ingredientMap.put(ingredient, Double.valueOf(volume));
-				}
-				if (!errorFlag && !ingredientMap.isEmpty()) {
-					drinkLibrary.addDrink(DrinkName, ingredientMap);
-				} else {
-					if (ingredientMap.isEmpty()) {
-						System.out.println("IGNORING DRINK");
-						System.out.println("Please enter ingredients for " + DrinkName);
-					}
-					errorFlag = false;
-				}
-			}
-		}
-		return drinkLibrary;
-	}
+                    Ingredient ingredient = drinkLibrary.addOrFindIngredient(ingredientName, 1, false);
+                    ingredientMap.put(ingredient, Double.valueOf(volume));
+                }
+                if (!errorFlag && !ingredientMap.isEmpty()) {
+                    drinkLibrary.addDrink(DrinkName, ingredientMap);
+                } else {
+                    if (ingredientMap.isEmpty()) {
+                        System.out.println("IGNORING DRINK");
+                        System.out.println("Please enter ingredients for " + DrinkName);
+                    }
+                    errorFlag = false;
+                }
+            }
+        }
+        return drinkLibrary;
+    }
 
-	private Ingredient addOrFindIngredient(String name, double density, boolean carbonated) {
-		if (!ingredients.containsKey(name)) {
-			Ingredient ingredient = new Ingredient(name, density, carbonated);
-			ingredients.put(name, ingredient);
-		}
+    private Ingredient addOrFindIngredient(String name, double density, boolean carbonated) {
+        if (!ingredients.containsKey(name)) {
+            Ingredient ingredient = new Ingredient(name, density, carbonated);
+            ingredients.put(name, ingredient);
+        }
 
-		return ingredients.get(name);
-	}
+        return ingredients.get(name);
+    }
 
-	public Drink getDrink(String drinkName) {
-		return drinks.get(drinkName);
-	}
+    public Drink getDrink(String drinkName) {
+        return drinks.get(drinkName);
+    }
 
-	public ObservableList<String> getDrinkList() {
-		ObservableList<String> drinkNames = FXCollections.observableArrayList(drinks.keySet());
-		FXCollections.sort(drinkNames);
-		return drinkNames;
-	}
+    public ObservableList<String> getDrinkList() {
+        ObservableList<String> drinkNames = FXCollections.observableArrayList(drinks.keySet());
+        FXCollections.sort(drinkNames);
+        return drinkNames;
+    }
 
-	public Ingredient getIngredient(String ingredient) {
-		return ingredients.get(ingredient);
-	}
+    public Ingredient getIngredient(String ingredient) {
+        return ingredients.get(ingredient);
+    }
 }
